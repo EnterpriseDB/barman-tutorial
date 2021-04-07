6fef7f87c38699777d7a29c4028673f555a7bebf
Josh Heyer <josh.heyer@enterprisedb.com>
2021-04-07 16:42:17 -0600

    init check for psql on step 4

--- build/step04/backup/initcheck-psql.sh
+++ build/step04/backup/initcheck-psql.sh
@@ -2,10 +2,10 @@
 
 # it's (just barely) possible to start running the scenario before 
 # the container is fully initialized (backups restored, etc) 
-# check for this and provide a helpful message, or forward on to real barman
+# check for this and provide a helpful message, or forward on to real psql
 if test -f "/var/lib/barman/pg.tar.gz"
 then
   echo "still initializing, try again!"
 else
-  exec /usr/bin/barman "$@"
+  exec /usr/bin/psql "$@"
 fi
